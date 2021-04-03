6665f887d3d4f2009b6f44ae3033cc319cf581f5
RossDuffy <17201624@studentmail.ul.ie>
2021-04-03 14:24:49 +0100

    Showing the flock constraint boundary

--- Scripts/Editor/2D/StayInRadiusBehaviourEditor.cs
+++ Scripts/Editor/2D/StayInRadiusBehaviourEditor.cs
@@ -8,7 +8,27 @@ public class StayInRadiusBehaviourEditor : Editor
 {
     public override void OnInspectorGUI()
     {
-
         DrawDefaultInspector();
+    }
+
+    private void OnEnable()
+    {
+        SceneView.duringSceneGui += OnSceneGUI;
+    }
+
+    private void OnDisable()
+    {
+        SceneView.duringSceneGui -= OnSceneGUI;
+    }
+
+    public void OnSceneGUI(SceneView sceneView)
+    {
+        StayInRadiusBehaviour targ = (StayInRadiusBehaviour)target;
+
+        Vector3 centre = (Vector3)targ.centre;
+        Vector3 normal = new Vector3(0f, 0f, 1f);
+        float radius = targ.radius;
+
+        Handles.DrawWireDisc(centre, normal, radius);
     }
 }
